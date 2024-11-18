4e2ac3af9437d254d8bd0093abce020500d11e13
Alex Torrance <hello@alextorrance.co.uk>
2024-11-18 23:04:03 +0000

    Ensure public css folder exists before writing file

--- compileCss.js
+++ compileCss.js
@@ -1,5 +1,5 @@
 import * as sass from 'sass';
-import { readdirSync , writeFileSync} from 'fs';
+import { readdirSync , writeFileSync, mkdirSync } from 'fs';
 import path from 'path';
 
 const compileCss = ({src, dest, ext=[".sass", ".scss"]}) => {
@@ -11,6 +11,7 @@ const compileCss = ({src, dest, ext=[".sass", ".scss"]}) => {
         const cssName = baseName + '.css';
         const destFilePath = path.join(dest, cssName);
         const result = sass.compile(srcFilePath);
+        mkdirSync(dest, { recursive: true });
         writeFileSync(destFilePath, result.css, 'utf-8');
     }
 
